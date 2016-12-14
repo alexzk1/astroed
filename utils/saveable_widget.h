fe052ba7ab539ebe45af92750e33bde1bdc0ed88
alexzk1 <alexzkhr@gmail.com>
2016-12-14 17:40:36 +0200

    changed restoring window order, pos/size will be restored at last for the Savable Widget

--- utils/saveable_widget.h
+++ utils/saveable_widget.h
@@ -5,68 +5,70 @@
 #include <QSettings>
 namespace utility
 {
-    class SaveableWidget
+class SaveableWidget
+{
+private:
+    struct LastSett
     {
-    private:
-        struct LastSett
-        {
-            QPoint pos;
-            QSize  size;
-        };
-        LastSett lastSett;
-    protected:
-        void readSettings(QWidget* window)
-        {
-            LastSett tmp;
-            tmp.size = window->size();
-            tmp.pos  = window->pos();
-            lastSett = tmp;
-
-            QSettings settings;
-
-            settings.beginGroup(window->objectName());
-            QVariant value = settings.value("pos");
-            if (!value.isNull())
-            {
-                window->move(settings.value("pos").toPoint());
-                window->resize(settings.value("size").toSize());
-            }
-            recurseRead(settings, window);
-            settings.endGroup();
-        }
-
-        void writeSettings(QWidget* window)
-        {
-            QSettings settings;
-
-            settings.beginGroup(window->objectName());
-            settings.setValue("pos", window->pos());
-            settings.setValue("size", window->size());
-            recurseWrite(settings, window);
-            settings.endGroup();
-            settings.sync();
-        }
-
-        virtual void recurseRead(QSettings& settings, QObject* object)
-        {
-            Q_UNUSED(settings)
-            Q_UNUSED(object)
-        }
-        virtual void recurseWrite(QSettings& settings, QObject* object)
-        {
-            Q_UNUSED(settings)
-            Q_UNUSED(object)
-        }
-        virtual ~SaveableWidget()
-        {
-        }
-
-    public:
-        void resetSizePos(QWidget* window)
-        {
-            LastSett tmp = lastSett;
-            window->move(tmp.pos);
-            window->resize(tmp.size);
-        }
+        QPoint pos;
+        QSize  size;
     };
+    LastSett lastSett;
+protected:
+    void readSettings(QWidget* window)
+    {
+        LastSett tmp;
+        tmp.size = window->size();
+        tmp.pos  = window->pos();
+        lastSett = tmp;
+
+        QSettings settings;
+
+        settings.beginGroup(window->objectName());
+        recurseRead(settings, window);
+
+        QVariant value = settings.value("pos");
+        if (!value.isNull())
+        {
+            window->move(settings.value("pos").toPoint());
+            window->resize(settings.value("size").toSize());
+        }
+
+        settings.endGroup();
+    }
+
+    void writeSettings(QWidget* window)
+    {
+        QSettings settings;
+
+        settings.beginGroup(window->objectName());
+        settings.setValue("pos", window->pos());
+        settings.setValue("size", window->size());
+        recurseWrite(settings, window);
+        settings.endGroup();
+        settings.sync();
+    }
+
+    virtual void recurseRead(QSettings& settings, QObject* object)
+    {
+        Q_UNUSED(settings)
+        Q_UNUSED(object)
+    }
+    virtual void recurseWrite(QSettings& settings, QObject* object)
+    {
+        Q_UNUSED(settings)
+        Q_UNUSED(object)
+    }
+    virtual ~SaveableWidget()
+    {
+    }
+
+public:
+    void resetSizePos(QWidget* window)
+    {
+        LastSett tmp = lastSett;
+        window->move(tmp.pos);
+        window->resize(tmp.size);
+    }
+};
 }
