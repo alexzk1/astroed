569f51479327e3a058fc90a0215a4c1d3634a0fb
alexzk1 <alexzkhr@gmail.com>
2017-06-21 14:02:50 +0300

    update of "union_cast" to do proper strict aliasing

--- utils/union_cast.h
+++ utils/union_cast.h
@@ -1,19 +1,22 @@
 #ifndef UNION_CAST_H
 #define UNION_CAST_H
+#include <memory.h>
+#include <type_traits>
+//tricking strict aliasing
 
-namespace utility
+template<class T, class Src>
+typename std::enable_if<std::is_pointer<T>::value, T>::type union_cast(Src src)
 {
-
-    template<class T, class Src>
-    T union_cast(Src src)
-    {
-        union
-        {
-            Src s;
-            T d;
-        } tmp;
-        tmp.s = src;
-        return tmp.d;
-    }
+//    union
+//    {
+//        Src s;
+//        T d;
+//    } tmp;
+//    tmp.s = src;
+//    return tmp.d;
+    T tmp;
+    memcpy(&tmp, &src, sizeof (tmp));
+    return tmp;
 }
+
 #endif // UNION_CAST_H
